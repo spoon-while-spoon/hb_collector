79b58ef19085d62f10a7eb1edd2303832ed11f34
spoon-while-spoon <tebagos@gmail.com>
2023-12-10 12:45:55 +0100

    Small changes
    added the following:
    - exeption Handling
    - logging added

--- collect_temp.py
+++ collect_temp.py
@@ -2,30 +2,40 @@ import requests
 import json
 import time
 from datetime import datetime, timedelta
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def login():
-    login_url = "http://192.168.2.20:8181/api/auth/login"
-    credentials = {"username": "admin", "password": "admin", "otp": "string"}
-    response = requests.post(login_url, json=credentials)
-    response.raise_for_status()
-    token_info = response.json()
-    return token_info['access_token'], datetime.now() + timedelta(seconds=token_info['expires_in'])
+    try:
+        login_url = "http://192.168.2.20:8181/api/auth/login"
+        credentials = {"username": "admin", "password": "admin", "otp": "string"}
+        response = requests.post(login_url, json=credentials)
+        response.raise_for_status()
+        token_info = response.json()
+        return token_info['access_token'], datetime.now() + timedelta(seconds=token_info['expires_in'] - 60) 
+    except requests.RequestException as e:
+        logging.error(f"Login fehlgeschlagen: {e}")
+        raise
 
 def query_homebridge_api(token):
-    url = "http://192.168.2.20:8181/api/accessories/34f747c785c7a0c34a651109d7ff14ca97622e7028a7a6587a9f7e8c4f41ec08"
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    data = response.json()
+    try:
+        url = "http://192.168.2.20:8181/api/accessories/34f747c785c7a0c34a651109d7ff14ca97622e7028a7a6587a9f7e8c4f41ec08"
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+    except requests.RequestException as e:
+        logging.error(f"API-Anfrage fehlgeschlagen: {e}")
 
+try:
+    token, token_expiry = login()
 
-token, token_expiry = login()
+    while True:
+        if datetime.now() >= token_expiry:
+            token, token_expiry = login()
 
-# API loop
-while True:
-    
-    if datetime.now() >= token_expiry:
-        token, token_expiry = login()
-
-    query_homebridge_api(token)
-    time.sleep(60)  # wait 60
+        query_homebridge_api(token)
+        time.sleep(60)
+except KeyboardInterrupt:
+    logging.info("Skript wurde manuell beendet.")
